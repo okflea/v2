fd16d6eebd4f50a8222845202c13e58479283a15
rohit <rohit@underpinservices.com>
2025-01-28 15:04:51 +0530

    CHORE: added gitignore

--- service2/src/index.ts
+++ service2/src/index.ts
@@ -9,7 +9,7 @@ const server = http.createServer(app.callback());
   try {
     await createSocketServer(server);
     server.listen(PORT, () => {
-      console.log(`Service running on port ${PORT}`);
+      console.log(`Service 2 running on port ${PORT}`);
     });
   } catch (err) {
     console.error('Failed to start the server:', err);
